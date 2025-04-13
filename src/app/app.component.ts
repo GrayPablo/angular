4913e177bb6b99f7be268dfb5e8c646296498142
Pablo Gray <pgrays.project@gmail.com>
2025-04-14 00:40:47 +0100

    This starts to look better...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomTextControlComponent } from './custom-text-control/custom-text-control.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +14,21 @@ export class AppComponent implements OnInit {
   
   title = 'custom-control-example';
   form = new FormGroup({
-    name: new FormControl(''),
-    surname: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    surname: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(10)]),
   });
   
   ngOnInit(): void {
   }
   
   submit() {
+    this.form.markAllAsTouched();
     console.log(this.form.controls.name.value + ',' + this.form.controls.surname.value);
 
+  }
+
+  get name() {    
+    return this.form.get('name');  
   }
 
 }
--- src/app/custom-text-control/custom-text-control.component.ts
+++ src/app/custom-text-control/custom-text-control.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { TitleCasePipe } from '@angular/common';
+import { Component, Input, Pipe } from '@angular/core';
+import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-text-control',
-  imports: [],
+  imports: [TitleCasePipe],
   template: `
   <div class="row">
     <div class="col">
-        <label class="form-label" for="surname"> Surname </label>
+        <label class="form-label" for="surname"> {{label | titlecase }} </label>
     </div>
     <div class="col">
         <input matInput [value]="value" type="text" class="form-control" name="surname" (input)="onInput($event)" (blur)="onTouched()"/>
-    </div>
+        @if (this.control?.touched) {
+          @if (this.control?.hasError('required')) {
+            <div>Required</div>
+          }
+          @if (this.control?.hasError('minlength')) {
+            <div>Too short</div>
+          }
+          @if (this.control?.hasError('maxlength')) {
+            <div>Too long</div>
+          }
+        }
+        
+      </div>
   </div>
   `,
   styleUrl: './custom-text-control.component.css',
@@ -21,15 +34,26 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
       provide: NG_VALUE_ACCESSOR,
       multi: true,
       useExisting: CustomTextControlComponent
+    },
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: CustomTextControlComponent
     }
   ]
 })
-export class CustomTextControlComponent implements ControlValueAccessor {
+export class CustomTextControlComponent implements ControlValueAccessor, Validator {
   
+  @Input()
+  label: string | undefined;
+
   value: string | undefined;
 
+  control: AbstractControl | undefined;
+  
   onChange = (value: string) => {};
   onTouched   = () => {};
+  onValidatorChange = () => {};
   
   writeValue(obj: string): void {
     this.value = obj;
@@ -43,11 +67,20 @@ export class CustomTextControlComponent implements ControlValueAccessor {
   // setDisabledState?(isDisabled: boolean): void {
     //   throw new Error('Method not implemented.');
     // }
-    onInput(e: Event) {
-      const input = e.target as HTMLInputElement;
-      this.value = input.value;
-      console.log('Input value is: ' + this.value)
-      this.onChange(this.value);
-    }
+  onInput(e: Event) {
+    const input = e.target as HTMLInputElement;
+    this.value = input.value;
+    // console.log('Input value is: ' + this.value)
+    this.onChange(this.value);
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    this.control = control;
+    return null;
+  }
+
+  registerOnValidatorChange(fn: () => void): void {
+    this.onValidatorChange = fn;
+  }
 
 }
